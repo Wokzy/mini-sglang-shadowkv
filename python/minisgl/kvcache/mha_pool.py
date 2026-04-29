da12ac0d6e518e86e280b8453e8c1f894e11e5eb
Wokzy <yegor6741@gmail.com>
2026-04-29 13:31:20 +0300

    false promise mha pool
    Signed-off-by: Wokzy <yegor6741@gmail.com>

--- python/minisgl/kvcache/mha_pool.py
+++ python/minisgl/kvcache/mha_pool.py
@@ -24,6 +24,7 @@ class MHAKVCache(BaseKVCachePool):
         page_size: int,
         dtype: torch.dtype,
         device: torch.device,
+        max_batch_size: int = None
     ) -> None:
         tp_info = get_tp_info()
         self.local_kv_heads = div_even(num_kv_heads, tp_info.size, allow_replicate=True)
@@ -33,6 +34,14 @@ class MHAKVCache(BaseKVCachePool):
             device=device,
             dtype=dtype,
         )
+
+        self.enable_fp8 = (dtype == torch.float8_e4m3fn)
+
+        if self.enable_fp8:
+            self.qmax = 448.0
+            self.k_inv_batch_scales = torch.ones((num_layers, max_batch_size), dtype=torch.bfloat16, device=self.device)
+            self.v_inv_batch_scales = torch.ones((num_layers, max_batch_size), dtype=torch.bfloat16, device=self.device)
+
 
         print(f"MHA POOL INIT: {num_pages=} {page_size=} {self._kv_buffer.shape}")
 
@@ -49,9 +58,39 @@ class MHAKVCache(BaseKVCachePool):
     def v_cache(self, index: int) -> torch.Tensor:
         return self._v_buffer[index]
 
+    def fp8_quantize(self, k, v, layer_idx: int, batch_indices) -> tuple[torch.Tensor, torch.Tensor]:
+        if len(batch_indices) == 0:
+            batch_idx = batch_indices[0]
+            self.k_inv_batch_scales[layer_idx, batch_idx] = torch.amax(k) / self.qmax
+            self.v_inv_batch_scales[layer_idx, batch_idx] = torch.amax(v) / self.qmax
+
+            return ((k / self.k_inv_batch_scales[layer_idx, batch_idx]).to(torch.float8_e4m3fn),
+                    (v / self.v_inv_batch_scales[layer_idx, batch_idx]).to(torch.float8_e4m3fn))
+
+        qmaxes = torch.ones_like(k[0], device=k.device) * self.qmax
+        k_res = []
+        v_res = []
+        for i, batch_idx in enumerate(batch_indices):
+            k_res.append(torch.min(k[i] / self.k_inv_batch_scales[layer_idx, batch_idx], qmaxes).to(torch.float8_e4m3fn))
+            v_res.append(torch.min(v[i] / self.k_inv_batch_scales[layer_idx, batch_idx], qmaxes).to(torch.float8_e4m3fn))
+
+        return torch.stack(k_res, dim=0), torch.stack(v_res, dim=0)
+
+    def fp8_dequantize(self, k, v, layer_idx: int, batch_indices) -> tuple[torch.Tensor, torch.Tensor]:
+        if len(batch_indices) == 0:
+            batch_idx = batch_indices[0]
+
+            return (k.to(torch.bfloat16) * self.k_inv_batch_scales[layer_idx, batch_idx],
+                    v.to(torch.bfloat16) * self.v_inv_batch_scales[layer_idx, batch_idx])
+
     def store_kv(
-        self, k: torch.Tensor, v: torch.Tensor, out_loc: torch.Tensor, layer_id: int
+        self, k: torch.Tensor, v: torch.Tensor, out_loc: torch.Tensor, layer_id: int, batch_indices: list[int] | None,
     ) -> None:
+
+        if self.enable_fp8:
+            assert batch_indices is not None
+            k, v = self.fp8_quantize(k, v, layer_id, batch_indices)
+
         store_cache(
             k_cache=self._k_buffer[layer_id].view(self._storage_shape),
             v_cache=self._v_buffer[layer_id].view(self._storage_shape),
